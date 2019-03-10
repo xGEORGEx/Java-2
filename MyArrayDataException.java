c4a81afbc0ec0670469017697eef8e3cd3d4897e
xGEORGEx <rox_2@mail.ru>
2019-03-10 14:20:42 +0400

    Задание к четвертому уроку

--- MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.J2L2;
-
-public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException(String message){
-        super(message);
-    }
-}
--- MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.J2L2;
-
-public class MyArraySizeException extends RuntimeException {
-    public MyArraySizeException(String message){
-        super(message);
-    }
-}
